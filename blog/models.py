85ff8bdade0d6995ea5a5a811490cff969d6567a
Anton Morozov <maa1@eis24.me>
2025-04-14 15:39:54 +0300

    Add PersonQuerySet, PersonManager and Person to models.py

--- blog/models.py
+++ blog/models.py
@@ -83,3 +83,32 @@ class Employee(models.Model):
         related_name='employees',
     )
     salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    
+class PersonQuerySet(models.QuerySet):
+    def authors(self):
+        return self.filter(role="A")
+
+    def editors(self):
+        return self.filter(role="E")
+
+        
+class PersonManager(models.Manager):
+    def get_queryset(self):
+        return PersonQuerySet(self.model, using=self._db)
+
+    def authors(self):
+        return self.get_queryset().authors()
+
+    def editors(self):
+        return self.get_queryset().editors()
+
+        
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(
+        max_length=1, 
+        choices={"A": _("Author"), "E": _("Editor")},
+    )
+    people = PersonManager()
